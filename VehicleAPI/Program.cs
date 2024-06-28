85ced51622859374c2eb1d8a0e1c4fc89267328a
Mümin Emincan KURNAZ <112796390+muminkurnaz@users.noreply.github.com>
2024-06-28 19:51:59 +0300

    Update Program.cs

--- VehicleAPI/Program.cs
+++ VehicleAPI/Program.cs
@@ -5,26 +5,19 @@ using VehicleAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Add VehicleRepository as a scoped service
 builder.Services.AddScoped<VehicleRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
